fee4f492b51021f32d354b56516c0e37dd004bf8
Wilknis <wilknisoliveira@gmail.com>
2025-10-26 17:16:43 -0300

    style: adding react-bootstrap lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+
 import Home from "./pages/home/home";
 
 createRoot(document.getElementById("root")!).render(
