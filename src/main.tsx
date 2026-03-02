ed98bf6fb0a8ad7157e201ca60e35a3d638292ab
Wilknis <wilknisoliveira@gmail.com>
2026-03-02 19:16:39 -0300

    config: adding scss to the project and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
+import "./styles/globalStyles.scss";
 
 import Home from "./pages/home/home";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>{<Home />}</StrictMode>
+  <StrictMode>{<Home />}</StrictMode>,
 );
