0035f5ebd20ce9953cf8b70efa97ee56d63b2062
Wilknis <wilknisoliveira@gmail.com>
2026-03-05 09:20:59 -0300

    style: simple dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from "react-router";
+import { useState } from "react";
 
 import Navbar from "./components/nav-bar/Navbar";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toogleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      document.body.style.backgroundColor = "dark";
+    } else {
+      setTheme("light");
+      document.body.style.backgroundColor = "light";
+    }
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar theme={theme} toogleTheme={toogleTheme} />
       <h1>App</h1>
       <Outlet />
       <footer>Footer</footer>
--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -1,14 +1,37 @@
 import { NavLink } from "react-router";
 
-const Navbar = () => {
+const Navbar = ({
+  theme,
+  toogleTheme,
+}: {
+  theme: string;
+  toogleTheme: () => void;
+}) => {
+  let textTheme = theme === "light" ? "dark" : "light";
   return (
-    <nav>
+    <nav className={`navbar bg-${theme}`}>
       <NavLink to="/" end>
         Home
       </NavLink>
       <NavLink to="/about" end>
         About
       </NavLink>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          role="switch"
+          id="switchCheckDefault"
+          onClick={toogleTheme}
+          style={{ cursor: "pointer" }}
+        />
+        <label
+          className={`form-check-label text-${textTheme}`}
+          htmlFor="switchCheckDefault"
+        >
+          {theme === "light" ? "Enable Dark Mode" : "Enable Light Mode"}
+        </label>
+      </div>
     </nav>
   );
 };
